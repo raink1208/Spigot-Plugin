a46746c6946ae5b1a46c6c23ee2580c6eaadf549
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-05-24 13:09:19 +0900

    銃の種類の追加 configでいじれるように

--- Gun/src/main/kotlin/com/rain1208/gun/Gun.kt
+++ Gun/src/main/kotlin/com/rain1208/gun/Gun.kt
@@ -3,26 +3,39 @@ package com.rain1208.gun
 import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.event.Listener
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 class Gun : JavaPlugin(), Listener {
+    companion object {
+        lateinit var instance:Gun
+        lateinit var configs:FileConfiguration
+        private set
+    }
     override fun onEnable() {
         logger.info("有効化します")
+        saveDefaultConfig()
+        configs = config
+        instance = this
         server.pluginManager.registerEvents(UseGun(),this)
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (label == "gun") {
             if (sender is Player) {
-                val item = ItemStack(Material.STICK)
-                val meta = item.itemMeta
-                meta!!.setDisplayName("gun")
-                item.itemMeta = meta
-                sender.inventory.addItem(item)
-                sender.sendMessage("gunを配布しました")
+                if (args[0].isNotEmpty()) {
+                    val item = ItemStack(Material.STICK)
+                    val meta = item.itemMeta
+                    meta!!.setDisplayName(args[0])
+                    item.itemMeta = meta
+                    sender.inventory.addItem(item)
+                    sender.sendMessage("gunを配布しました")
+                } else {
+                    sender.sendMessage("/gun <gunName>")
+                }
             }
         }
         return true
--- Gun/src/main/kotlin/com/rain1208/gun/UseGun.kt
+++ Gun/src/main/kotlin/com/rain1208/gun/UseGun.kt
@@ -13,7 +13,7 @@ import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 
 class UseGun:Listener {
-    @EventHandler
+    /*@EventHandler
     fun useGun(ev: PlayerInteractEvent) {
         if (ev.action == Action.RIGHT_CLICK_AIR || ev.action == Action.RIGHT_CLICK_BLOCK) {
             if (ev.item != null && ev.item!!.type == Material.STICK && ev.item!!.itemMeta?.displayName == "gun") {
@@ -26,7 +26,7 @@ class UseGun:Listener {
                     pos.world!!.playSound(pos, Sound.ENTITY_FIREWORK_ROCKET_BLAST,0.1f,1.0f) //パーティクル上
                     ev.player.world.spawnParticle(Particle.VILLAGER_ANGRY,pos, 1, 0.02, 0.02, 0.02, 4.0)
 
-                    for (entity in getEntitiesByPos(pos,0.55f)) {
+                    for (entity in getEntitiesByPos(pos,0.6)) {
                         if (entity is LivingEntity) {
                             entity.damage(5.0) //ダメージ量
                         }
@@ -37,12 +37,53 @@ class UseGun:Listener {
                 }
             }
         }
+    }*/
+
+    private val effect = arrayOf(
+            Particle.VILLAGER_HAPPY,
+            Particle.VILLAGER_ANGRY
+    )
+
+    @EventHandler
+    fun useGun(ev:PlayerInteractEvent) {
+        if (ev.action == Action.RIGHT_CLICK_AIR || ev.action == Action.RIGHT_CLICK_BLOCK) {
+            if (ev.item != null && ev.item!!.type == Material.STICK) {
+                val item = ev.item?.itemMeta!!.displayName
+                val config = Gun.configs
+                if (config.contains(item)) {
+                    val range = config.get("${item}.range") as Int
+                    val e = config.get("${item}.effect") as Int
+                    val damage = config.get("${item}.damage") as Double
+                    val radius = config.get("${item}.radius") as Double
+                    val p: Player = ev.player
+                    var pos: Location = p.location
+                    pos.y++
+                    pos.world!!.playSound(pos,Sound.ENTITY_FIREWORK_ROCKET_LARGE_BLAST,1.0f,0.1f)
+                    for (i in 0..range) {
+                        pos = pos.add(pos.direction.x,pos.direction.y-0.005f,pos.direction.z)
+                        pos.world!!.playSound(pos,Sound.ENTITY_FIREWORK_ROCKET_SHOOT,0.1f,10.0f)
+                        p.world.spawnParticle(effect[e],pos,1,0.02,0.02,0.02,4.0)
+
+                        for (entity in getEntitiesByPos(pos,radius)) {
+                            if (entity is LivingEntity) {
+                                entity.damage(damage)
+                            }
+                        }
+                        if (pos.block.type.isSolid) {
+                            break
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    private fun getEntitiesByPos(pos:Location, r:Float):ArrayList<Entity> {
+    private fun getEntitiesByPos(pos:Location, r:Double):ArrayList<Entity> {
         val entity: ArrayList<Entity> = ArrayList()
         for (e in pos.world!!.entities) {
-            if (e.location.distanceSquared(pos) <= r) {
+            val p = e.location
+            p.y++
+            if (p.distanceSquared(pos) <= r) {
                 entity.add(e)
             }
         }
